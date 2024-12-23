dbe912f6d3088258aeb9a6f19b7997577f22f599
Ysen <wilson.xue9310@gmail.com>
2024-12-23 12:36:03 +0800

    feat:init project

---
+++ InsPlan/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  InsPlan
+//
+//  Created by Ysen on 2024/12/23
+//  Copyright © 2024 Ysen. All rights reserved.
+//
+    
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ InsPlan/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  InsPlan
+//
+//  Created by Ysen on 2024/12/23
+//  Copyright © 2024 Ysen. All rights reserved.
+//
+    
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
